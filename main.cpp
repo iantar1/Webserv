24cc4d410f300e09eac036914310af1aa9ed7932
test <test@test.com>
2024-02-15 01:13:46 +0100

    Auto Commit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: nabboune <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:07:55 by nabboune          #+#    #+#             */
-/*   Updated: 2024/02/13 23:41:15 by nabboune         ###   ########.fr       */
+/*   Updated: 2024/02/15 01:12:05 by nabboune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include <unistd.h>
 #include <sstream>
 #include <map>
+#include <ctime>
 
 #define	PORT 8080
 
@@ -45,7 +46,8 @@ char	*parse(char *src)
 	return dest;
 }
 
-std::string ToString(std::size_t num)
+template <typename T>
+std::string ToString(T num)
 {
 	std::stringstream ss;
 	ss << num;
@@ -57,14 +59,18 @@ std::map<std::string, std::string>	requestParse(char *buffer)
 	int									i = 0;
 	std::string							key, value;
 	std::map<std::string, std::string>	output;
+	std::map<std::string, std::string>::iterator	it;
 
 	key = "";
 	value = "";
 	while(buffer[i] && buffer[i] != ' ')
 		key += buffer[i++];
-	i++;
+	i += 2;
 	while(buffer[i] && buffer[i] != ' ' && buffer[i] != '\n')
 		value += buffer[i++];
+	while(buffer[i] && buffer[i] != '\n')
+		i++;
+	i++;
 	output.insert(std::make_pair(key, value));
 	while(buffer[i])
 	{
@@ -85,43 +91,146 @@ std::map<std::string, std::string>	requestParse(char *buffer)
 			}
 		}
 		i++;
-		output.insert(std::make_pair(key, value));
+		if (key != "" && value != "")
+			output.insert(std::make_pair(key, value));
 	}
 	return (output);
 }
 
-std::string	getContentType(std::string file)
+std::map<std::string, std::string>::iterator	easyfind(std::map<std::string, std::string> &container, std::string x)
 {
-	std::string	out;
-	std::size_t	point = file.find('.');
-
-	if(point == std::string::npos)
+	std::map<std::string, std::string>::iterator	it = container.begin();
+	while (it != container.end())
 	{
-		out = "text/plain";
-		return (out);
+		if (it->first == x)
+			return it;
+		it++;
 	}
-	// Get content type ".jpg => Content-Type: Image/jpg"
+	return it;
 }
 
-std::string	constructRequestHeader(std::map<std::string, std::string> dictionary)
+std::map<std::string, std::string>	mimeTypes(void)
 {
-	std::map<std::string, std::string>::iterator	it = map.begin();
-	std::string										header, body, line;
+	std::map<std::string, std::string>	out;
+	std::ifstream						inFile("MIME.type");
+	std::string							line, key, value;
+	int									i;
 
-	if(it->first = "GET")
+	if (!inFile.is_open())
+		std::cout << "Couldn't open MIME.type file!" << std::cout;
+	while (std::getline(inFile, line))
 	{
-		std::ifstream	inFile(it->second);
+		key = "";
+		value = "";
+		i = 0;
+		while((line.c_str())[i] && (line.c_str())[i] != ' ')
+			key += (line.c_str())[i++];
+		i++;
+		while((line.c_str())[i] && (line.c_str())[i] != '\n')
+			value += (line.c_str())[i++];
+		out.insert(std::make_pair(key, value));
+	}
+	return out;
+}
+
+std::string dec_to_hex(int decimal) {
+    char	buffer[50];
+    sprintf(buffer, "%X", decimal);
+    return std::string(buffer);
+}
+
+void	constructResponse(int socket, std::map<std::string, std::string> dictionary, std::map<std::string, std::string> mime)
+{
+	std::map<std::string, std::string>::iterator	it = dictionary.begin();
+	std::map<std::string, std::string>::iterator	response_it, mime_it;
+	std::map<std::string, std::string>				response;
+	std::stringstream								chunkStream;
+	std::string										header, body, line, out;
+
+	response.insert(std::make_pair("0", "HTTP/1.1 "));
+	response.insert(std::make_pair("1", "Content-Type: "));
+	// response.insert(std::make_pair("2", "Content-Length: "));
+	response.insert(std::make_pair("2", "Transfer-Encoding: "));
+	response.insert(std::make_pair("3", "Date: "));
+	response.insert(std::make_pair("4", "Server: Muguiwarra"));
+	while(it != dictionary.end())
+	{
+		if(it->first == "GET" || it->first == "POST" || it->first == "DELETE")
+			break;
+		it++;
+	}
+	if(it != dictionary.end() && it->first == "GET")
+	{
+		time_t	now = time(0);
+		std::ifstream	inFile((it->second).c_str());
+		mime_it = easyfind(mime, it->second);
+		
+		/*
+			Get the extension of the file and then save the content type
+		*/
+		
+		// std::cout << mime_it->first << " || " << mime_it->second << std::endl;
+		// std::cout << it->second << std::endl;
+		std::string		contentType = mime_it->second;
+		// std::cout << contentType << std::endl;
+		tm	*local_time = localtime(&now);
+		std::string	strTime = ToString(local_time->tm_year + 1900) + "-" + ToString(local_time->tm_mon + 1) + "-" + ToString(local_time->tm_mday) + " " + ToString(local_time->tm_hour) + ":" + ToString(local_time->tm_min) + ":" + ToString(local_time->tm_sec);
+		header = "";
 		if (!inFile.is_open())
-			header = "HTTP/1.1 404 Not Found\nContent-Type: text/plain\nContent-Length: 10\n\nError 404!";
+		{
+			response_it = easyfind(response, "0");
+			response_it->second += "404 Not Found";
+			response_it = easyfind(response, "1");
+			response_it->second += "text/plain";
+			response_it = easyfind(response, "2");
+			// response_it->second += ToString(body.size());
+			response_it->second += "chunked";
+			response_it = easyfind(response, "3");
+			response_it->second += strTime;
+
+			response_it = response.begin();
+			while (response_it != response.end())
+			{
+				header += response_it->second + "\r\n";
+				response_it++;
+			}
+			write(socket, header.c_str(), header.size());
+
+			line = "Error 404!";
+			body = dec_to_hex(line.size()) + "\r\n" + line + "\r\n";
+			write(socket, body.c_str(), body.size());
+		}
 		else
 		{
+			response_it = easyfind(response, "0");
+			response_it->second += "200 OK";
+			response_it = easyfind(response, "1");
+			response_it->second += contentType;
+			response_it = easyfind(response, "2");
+			// response_it->second += ToString(body.size());
+			response_it->second += "chunked";
+			response_it = easyfind(response, "3");
+			response_it->second += strTime;
+
+			response_it = response.begin();
+			while (response_it != response.end())
+			{
+				header += response_it->second + "\r\n";
+				response_it++;
+			}
+			write(socket, header.c_str(), header.size());
+
+			std::cout << "HEADER : \n" << header << "\n====" <<std::endl;
+
 			body = "";
 			while (std::getline(inFile, line))
-				body += line + "\n";
-			header = "HTTP/1.1 200 OK\nContent-Type: " + ?? + ToString(body.size()) + "\n\n" + body;
-			// Construct the response header
+			{
+				line = line + "\n";
+				body = dec_to_hex(line.size()) + "\r\n" + line + "\r\n";				
+				write(socket, body.c_str(), body.size());
+			}
 		}
-
+		write(socket, "0\r\n\r\n", 5);
 	}
 }
 
@@ -153,12 +262,9 @@ int	main(void)
 	if (listen(server_fd, 3) < 0)
 		return (std::cout << "listen failed" << std::endl, -1);
 
-	std::string	okHeaderText = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ";
-	std::string	error = "HTTP/1.1 404 Not Found\nContent-Type: text/plain\nContent-Length: 10\n\nError 404!";
-
 	std::string	line, body, response;
-	char	*file = NULL;
-	std::map	dictionary;
+	std::map<std::string, std::string>	dictionary;
+	std::map<std::string, std::string>	mime = mimeTypes();
 	while(true)
 	{
 		std::cout << "\n+++++++ Waiting for new connection ++++++++\n" << std::endl;
@@ -170,24 +276,15 @@ int	main(void)
 		int valread = read(new_socket, buffer, 4096);
 
 		std::cout << buffer << std::endl;
-		file = parse(buffer);
-		std::cout << "FILE : " << file << std::endl;
 
 		if(valread < 0)
 			std::cout << "No bytes are there to read" << std::endl;
 
-		std::ifstream	inFile(file);
-		if (!inFile.is_open())
-			write(new_socket, error.c_str(), error.size());
-		else
-		{
-			body = "";
-			while (std::getline(inFile, line))
-				body += line + "\n";
-			response = okHeaderText + ToString(body.size()) + "\n\n" + body;
-		}
-		std::cout << response.c_str() << "{"<<response.size()<<"}" << std::endl;
-		write(new_socket, response.c_str(), response.size());
+		dictionary = requestParse(buffer);
+		constructResponse(new_socket, dictionary, mime);
+
+		// std::cout << response.c_str() << "{"<<response.size()<<"}" << std::endl;
+		// write(new_socket, response.c_str(), response.size());
 		std::cout << "------------------Hello message sent-------------------\n" << std::endl;
 		close(new_socket);
 	}
