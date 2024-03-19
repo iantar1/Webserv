555e199b9934acc5d25d264a1eb86b096b83808a
Ismail Antar <antartalha@gmail.com>
2024-03-19 23:40:15 +0000

    to save

---
+++ src/request_Utils.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   request_Utils.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iantar <iantar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 23:30:16 by iantar            #+#    #+#             */
+/*   Updated: 2024/03/19 23:39:05 by iantar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../includes/headers.hpp"
+#include "../includes/Request.hpp"
+#include "../includes/utils.hpp"
+
+const std::string hex = "0123456789abcdef0123456789ABCDEF";
+
+int convert(char a, char b)
+{
+    int res;
+
+    res = (hex.find(a)) % 16;
+    res = (res<<4);
+    res += (hex.find(b)) % 16;
+    return (res);
+}
+
+bool    isHex(char a, char b)
+{
+    return (hex.find(a) != std::string::npos && hex.find(b) != std::string::npos);
+}
+
+void    encoding(std::string& str)
+{
+    size_t index;
+
+    for (size_t i = 0; i < str.size(); i++)
+    {
+        if ((index = str.find("%")) != str.size() - 3
+            && isHex(str[index + 1], str[index + 2]))
+        {
+            str = str.substr(0, index)
+                + (char)(convert(str[index + 1], str[index + 2]))
+                    + str.substr(index + 3);
+        }
+    }
+}
