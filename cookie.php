b8a370c72d8e011cb3d7b2e15c86a0c71d75c73b
test <test@test.com>
2024-03-30 11:08:43 +0000

    3awdou zmr mn 0

--- cookie.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Set a cookie named "user" with the value "ismail antar" that expires in 1 hour
-setcookie("user", "ismail antar", time() + 3600, "/");
-
-// Display a message indicating that the cookie has been set
-echo "Cookie 'user' is set!";
-?>
--- timeout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "3aaa"
-while :
-do
-    # Empty loop body
-    :
-done
